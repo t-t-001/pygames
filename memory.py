de4dc3f27e9cc03ec94178017e336afe54d59751
t-t-001 <t_toshiyuki@td5.so-net.ne.jp>
2022-05-04 18:03:20 +0900

    途中２

--- memory.py
+++ memory.py
@@ -1,28 +1,13 @@
-from itertools import count
+from email import message
 import pgzrun
+import time
+import random
+import math
 
-count = 0
-turn_card = []
-wait_flg = False
-WIDTH  = 610
-HEIGHT = 410
-card_w = 140
-card_h = 190
-
-card_b  = 'cardback_blue5'
-card = []
-x = 10
-y = 10
-m = 10
-w = 4
-h = 2
-
-for i in range(h):
-    for n in range(w):
-        card.append(Actor(card_b,topleft=(x,y)))
-        x += card_w + m
-    x = m
-    y += card_h + m
+WIDTH  = 610  #ウィンドウの横幅
+HEIGHT = 410  #ウィンドウの縦幅
+card_w = 140  #カードの横幅
+card_h = 190  #カードの縦幅
 
 #カードの表のリスト生成とシャッフル
 card_f = []
@@ -31,12 +16,40 @@ for i in range(1,5):
     card_f.append('{:02}'.format(i) + 'cardhearts')
 random.shuffle(card_f)
 
+#カードの裏
+card_b  = 'cardback_blue5'
+
+card      = []    #カードのActor
+count     =  0    #めくった順番
+total     =  0    #2回めくった回数
+turn_card = []    #めくったカード
+wait_flg  = False #待ち時間中はTrue 
+
+x         = 10    #カード配置用のx座標
+y         = 10    #カード配置用のy座標
+
+#カードの生成と配置座標のセット
+for i in range(2):
+    for n in range(4):
+        card.append(Actor(card_b,topleft=(x,y)))
+        x += card_w + 10
+    x = 10
+    y += card_h + 10
 
 #ウィンドウ内の描画
 def draw():
     screen.clear()
+    if point == len(card)/2:
+        message  =  ['Score','Charenge：' +  str(total),'Rate：' + \
+            str(math.floor(point/total*100)) + '%']
+        for i in range(3):
+            
+
+
+
     for c in card:
         c.draw()
+
 #マウスクリックの処理
 def on_mouse_up(pos):
     global count
