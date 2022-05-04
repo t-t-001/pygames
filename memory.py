9b05590ae3ca0d869ff2b331bf24180bc2c6dfd4
t-t-001 <t_toshiyuki@td5.so-net.ne.jp>
2022-05-04 22:03:38 +0900

    ２日目終了

--- memory.py
+++ memory.py
@@ -1,8 +1,11 @@
+from ctypes import pointer
 from email import message
 import pgzrun
 import time
 import random
 import math
+
+from sqlalchemy import null
 
 WIDTH  = 610  #ウィンドウの横幅
 HEIGHT = 410  #ウィンドウの縦幅
@@ -24,6 +27,7 @@ count     =  0    #めくった順番
 total     =  0    #2回めくった回数
 turn_card = []    #めくったカード
 wait_flg  = False #待ち時間中はTrue 
+point     =  0    #数字が揃うと1ポイント
 
 x         = 10    #カード配置用のx座標
 y         = 10    #カード配置用のy座標
@@ -40,40 +44,43 @@ for i in range(2):
 def draw():
     screen.clear()
     if point == len(card)/2:
-        message  =  ['Score','Charenge：' +  str(total),'Rate：' + \
-            str(math.floor(point/total*100)) + '%']
+        message  =  ['Score','Charenge:' +  str(total),'Rate:' +  str(math.floor(point/total*100)) + '%']
         for i in range(3):
-            
-
-
-
-    for c in card:
-        c.draw()
+            screen.draw.text(message[i],(100,150+i*50), color='orange',fontsize=48)
+    else:
+        for c in card:
+            c.draw()
 
 #マウスクリックの処理
-def on_mouse_up(pos):
+def on_mouse_down(pos):
     global count
     global turn_card
     global wait_flg
     if wait_flg:
         return
-    for c in card:
-        if c.collidepoint(pos):
-            c.image = '01cardclubs'
-            turn_card.append(c) #めくったカードを記録
-            count += 1
+    for i in range(len(card)):
+        if card[i].collidepoint(pos):
+            if card[i].image == card_b:
+                card[i].image = card_f[i] #カードを表にする
+                turn_card.append(card[i]) #めくったカードを記録
+                count += 1
     if count == 2:
         count = 0
         clock.schedule_unique(restore,1)
         wait_flg = True  #マウスクリックを無効化
 #1秒後にカードを裏に戻す関数
 def restore():
+    global point
     global turn_card
     global wait_flg
-    turn_card[0].image  =  card_b
-    turn_card[1].image  =  card_b
+    global total
+    #違う数字ならカードを裏返す
+    if turn_card[0].image[:2] != turn_card[1].image[:2]:
+        turn_card[0].image = card_b
+        turn_card[1].image = card_b
+    else:
+        point += 1 #同じ数字なら裏返さずにポイントを加算
+    total += 1 #2回めくった回数を増やす
     turn_card.clear()
     wait_flg = False
-
-
 pgzrun.go()
