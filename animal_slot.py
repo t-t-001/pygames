99cc9af3aad422fd5a0ed6a6bd866a50ebc76693
t-t-001 <t_toshiyuki@td5.so-net.ne.jp>
2022-06-10 21:16:16 +0900

    三日目Part3終了

--- animal_slot.py
+++ animal_slot.py
@@ -29,17 +29,15 @@ drum = [1,1,1]         # 0:回転中のドラム、1:停止しているドラム
 def draw():
     screen.clear()    #ウィンドウのクリア
     for i in range(3):
-
-
-
+        wheel[i][slot_count[i][0]].draw()
+        wheel[i][slot_count[i][1]].draw()
+    mask.draw()       #オブジェクトの描画　マスク
     switch.draw()     #オブジェクトの描画　棒
     ball.draw()       #オブジェクトの描画　棒の上のボール
     for i in range(3):
-        picture[i].draw()         #オブジェクトの描画　絵柄
         stop_button[i].draw()     #オブジェクトの描画　ストップボタン
 
 def on_mouse_down(pos):
-    global slot_count
     if ball.collidepoint(pos):                     #マウスダウン時、ボールとマウスが衝突してたら
         for i in range(3):
             drum[i] = 0                            #３つのドラムの状態を回転中にする
@@ -49,17 +47,22 @@ def on_mouse_down(pos):
                 drum[n] = 1                        #衝突したボタンの状態を停止にする
 
 def update():                                     #update関数は、1秒に60回呼び出される
+    for i in range(3):
+        if drum[i]  == 0:
+            rotation(i)
+
+def rotation(num):
     global slot_count
-    global time_count
-    time_count += 1                               #1秒間に60回足される
-    if time_count == 20:                          #この回数になったら
-        time_count = 0                            #カウンタ初期化
-        for i in range(3):                        #3つのドラム分繰り返し
-            if drum[i]  == 0:                     #回転中のドラムだったら
-                slot_count[i] += 1                #スロットの絵柄を順々に変更
-                if slot_count[i]  == 8:           #絵柄は1〜7の7種類なので、8になったら1に初期化
-                    slot_count[i]  =  1
-                # slot_count[i] = random.randrange(1, 7)  #絵柄をランダムに変更してみた
-                picture[i].image  =  'slot0' + str(slot_count[i])    #絵柄イメージを変更
+    wheel[num][slot_count[num][0]].y  +=  1
+    wheel[num][slot_count[num][1]].y  +=  1
+    if wheel[num][slot_count[num][0]].y > 180:
+        slot_count[num][0]  +=  1
+        slot_count[num][1]  +=  1
+        if slot_count[num][0]  == 6:
+            slot_count[num][1]  =  0
+        elif slot_count[num][0]  == 7:
+            slot_count[num][0]  =  0
+        wheel[num][slot_count[num][0]].y  =  80
+        wheel[num][slot_count[num][1]].y  =  -20
 pgzrun.go()
 
